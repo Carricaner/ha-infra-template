08c2c8ca3f6a7d2f0c0f509425a6fe566314a107
Darren <carricanbuild@gmail.com>
2024-12-18 21:40:22 -0500

    Modify VPC

--- envs/dev/vpc.tf
+++ envs/dev/vpc.tf
@@ -49,3 +49,47 @@ resource "aws_route_table" "public_subnet_route_table" {
     Name = "${local.name_prefix}_public_subnet_route_table"
   }
 }
+
+resource "aws_route_table_association" "public_subnet_association" {
+  route_table_id = aws_route_table.public_subnet_route_table.id
+  count          = length(local.available_zones)
+  subnet_id      = element(aws_subnet.public_subnet[*].id, count.index)
+}
+
+resource "aws_eip" "eip" {
+  domain = "vpc"
+  depends_on = [
+    aws_internet_gateway.internet_gateway
+  ]
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = element(aws_subnet.public_subnet[*].id, 0)
+  depends_on = [
+    aws_internet_gateway.internet_gateway
+  ]
+  tags = {
+    Name = "${local.name_prefix}_nat_gateway"
+  }
+}
+
+resource "aws_route_table" "private_subnet_route_table" {
+  vpc_id = aws_vpc.ha_template_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+  depends_on = [
+    aws_nat_gateway.nat_gateway
+  ]
+  tags = {
+    Name = "${local.name_prefix}_private_subnet_route_table"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  route_table_id = aws_route_table.private_subnet_route_table.id
+  count          = length(local.available_zones)
+  subnet_id      = element(aws_subnet.private_subnet[*].id, count.index)
+}
